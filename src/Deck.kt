ed727327bd7a84f9f01f4f86db842e6718c3e2ca
tahmasbi@bdood.ir <#1Burnitdown>
2019-03-09 14:45:30 +0330

    01-09

--- src/Deck.kt
+++ src/Deck.kt
@@ -1,6 +1,16 @@
+import java.util.*
+
 class Deck {
 
     val cards = Array(52, { Card(it % 13, getSuit(it)) })
+    var cardsInDeck : MutableList<Card> = cards.toMutableList()
+
+    fun drawCard(): Card = cardsInDeck.removeAt(0)
+
+    fun reset(){
+        cardsInDeck = cards.toMutableList()
+        cardsInDeck.shuffle()
+    }
 
     private fun getSuit(index: Int) =
         when (index / 13) {
